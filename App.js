ac06542ff94c855a18146ca9a0c19b7136af4d20
Ivaylo <ivayloivan0v@icloud.com>
2022-09-07 15:33:41 +0300

    setting up the app and outputting meal categories

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { CategoriesScreen } from "./screens/CategoriesScreen";
+
+export default function App() {
+  return <CategoriesScreen />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
