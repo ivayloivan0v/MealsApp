9046900ddb649d8c11aaefc1a9703a008edfb816
Ivaylo <ivayloivan0v@icloud.com>
2022-09-07 16:07:56 +0300

    displaying items in a grid

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet } from "react-native";
 import { CategoriesScreen } from "./screens/CategoriesScreen";
 
 export default function App() {
-  return <CategoriesScreen />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <CategoriesScreen />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
