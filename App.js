7e0f505ac407df761bf7c658613afce3963f4115
Ivaylo <ivayloivan0v@icloud.com>
2022-09-09 00:36:06 +0300

    setting navigation options dynamically

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
 
   return (
     <>
-      <StatusBar style="dark" />
+      <StatusBar style="light" />
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import { MealItem } from "../components/MealItem";
-import { MEALS } from "../data/dummy-data";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
-export const MealsOverviewScreen = ({ route }) => {
+export const MealsOverviewScreen = ({ route, navigation }) => {
   const catID = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catID) >= 0;
   });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catID
+    ).title;
+    navigation.setOptions({ title: categoryTitle });
+  }, [catID, navigation]);
 
   const renderMealItem = (itemData) => {
     const item = itemData.item;
